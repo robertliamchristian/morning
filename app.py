a123e73c3d5049169dffb74d29484fc979d92f99
Robbie Christian <robbiechristian@Robbies-MacBook-Air.local>
2023-12-13 23:14:34 -0800

    added tasksm update to UI

--- app.py
+++ app.py
@@ -87,15 +87,71 @@ def get_random_food():
         'dinner': random.choice(dinner_options)
     }
 
+def get_random_tasks():
+    task_options = ["clean litter", "walk", "meditate", "yoga", "play guitar", "call a friend", "send Lindsay flowers"]
+    
+    return {
+        'task': random.choice(task_options)
+    }
+
+def fetch_daily_stock_data(symbol):
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": symbol,
+        "outputsize": "compact",
+        "datatype": "json"
+    }
+    headers = {
+        "X-RapidAPI-Key": "d49e56dcbemsh75dcc891664b5a7p1cb502jsnaab489024d84",
+        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    # Check if 'Time Series (Daily)' key exists in the response
+    if 'Time Series (Daily)' in data:
+        df = pd.DataFrame(data['Time Series (Daily)']).T
+        df.reset_index(inplace=True)
+        df.rename(columns={
+            'index': 'Date',
+            '1. open': 'Open',
+            '2. high': 'High',
+            '3. low': 'Low',
+            '4. close': 'Close',
+            '5. volume': 'Volume'
+        }, inplace=True)
+        df['Symbol'] = symbol
+        return df
+    else:
+        print(f"No data for symbol: {symbol}")
+        return pd.DataFrame()
+
+def get_daily_stock_data():
+    symbols = ['CNC', 'UHG', 'GCMG','CVS','CI','PFE','CRSP']
+    all_data = []
+
+    for symbol in symbols:
+        stock_data = fetch_daily_stock_data(symbol)
+        if not stock_data.empty:
+            # Sort the DataFrame by date and get the top 5 rows for the symbol
+            top_5_data = stock_data.sort_values(by='Date', ascending=False).head(3)
+            all_data.append(top_5_data)
+
+    combined_df = pd.concat(all_data)
+    return combined_df
 
 def morning_update(weather_api_key, football_api_key):
     # Fetch weather data
     temp, weather_description, temp_high, temp_low, sunrise, sunset = fetch_weather(weather_api_key)
     # Fetch football matches data
     football_data = fetch_football_predictions(football_api_key)
+    # Fetch stock data
+    daily_stock_data = get_daily_stock_data()
     # Fetch date information
     today_date, days_new_year, days_birthday, days_lindsay_birthday = morning_update_dates()
     food_recommendations = get_random_food()
+    task_recommendations = get_random_tasks()
     
     # Adjust the context for the template
     context = {
@@ -110,18 +166,18 @@ def morning_update(weather_api_key, football_api_key):
         'days_birthday': days_birthday,
         'days_lindsay_birthday': days_lindsay_birthday,
         'matches_data': football_data.to_dict(orient='records'),  # Convert DataFrame to list of dicts
-        'food_for_today': food_recommendations
+        'food_for_today': food_recommendations,
+        'task_for_today': task_recommendations,
+        'daily_stock_data': daily_stock_data.to_html(index=False, classes='stock-table')
     }
-    
     # Render the template
     rendered_html = render_template(context)
-    
-    # Write the rendered HTML to a file
     with open('rendered_morning_update.html', 'w') as file:
         file.write(rendered_html)
+
+    print("Good Morning, Robbie! Report is ready to refresh")
+
     
-    
-    print("Morning update is ready in 'rendered_morning_update.html'.")
 
 
 
---
+++ stock.py
@@ -0,0 +1,33 @@
+def fetch_daily_stock_data(symbol):
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": symbol,
+        "outputsize": "compact",
+        "datatype": "json"
+    }
+    headers = {
+        "X-RapidAPI-Key": "d49e56dcbemsh75dcc891664b5a7p1cb502jsnaab489024d84",
+        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    # Check if 'Time Series (Daily)' key exists in the response
+    if 'Time Series (Daily)' in data:
+        df = pd.DataFrame(data['Time Series (Daily)']).T
+        df.reset_index(inplace=True)
+        df.rename(columns={
+            'index': 'Date',
+            '1. open': 'Open',
+            '2. high': 'High',
+            '3. low': 'Low',
+            '4. close': 'Close',
+            '5. volume': 'Volume'
+        }, inplace=True)
+        df['Symbol'] = symbol
+        return df
+    else:
+        print(f"No data for symbol: {symbol}")
+        return pd.DataFrame()
+        print(df)
